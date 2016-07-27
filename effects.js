8e5348a032f328d71caec3e243963720e64da9a9
Tzili Hod <tzili@Tzilis-MacBook-Air.local>
2016-07-27 06:43:16 -0700

    add toggle effect

---
+++ effects.js
@@ -0,0 +1,11 @@
+$("#about-section").click(function() {
+		$('#ski-smile, #about-text').slideToggle();
+	});
+
+	$("#experience").click(function() {
+		$('#exp-text').slideToggle();
+	});
+
+	$('#project-section').click(function() {
+		$('#projects').slideToggle();
+	});
